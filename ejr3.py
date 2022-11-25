4ec90b5707e1aa80ebe125e05b285f81e52b2302
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2022-11-25 11:23:40 +0100

    maravillas

---
+++ ejr3.py
@@ -0,0 +1,13 @@
+class Maravilla:
+    def __init__(self, nombre, lugar, tipo):
+        self.nombre = nombre
+        self.lugar = lugar
+        self.tipo = tipo
+
+chichen_itza = Maravilla("Chichen Itza", "Mexico", "Arquitectonica")
+muralla_china = Maravilla("Gran Muralla China", "China", "Arquitectonica")
+ciudad_petra = Maravilla("Ciudad de Petra", "Jordania", "Arquitectonica")
+taj_mahal = Maravilla("Taj Mahal", "India", "Arquitectonica")
+machu_pichu = Maravilla("Machu Pichu", "Peru", "Arquitectonica")
+coliseo = Maravilla("Coliseo de Roma", "Italia", "Arquitectonica")
+cristo_redentor = Maravilla("Estatua de Cristo Redentor", "Brasil", "Arquitectonica")
